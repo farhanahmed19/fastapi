3094d83c1c1aee3614c0a28f7674f25eddca5564
farhanahmed19 <shaikhfarhanahmed19@gmail.com>
2025-04-05 14:51:14 +0500

    New CI/CD Code Updated

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello Farhan Ahmed, Welcome to the world of CI/CD"}
-
+    return {"message": "Hello Farhan Ahmed Shaikh, Welcome to the world of CI/CD. Happy Learning!"}
